2eaa1b21e3408d1e58a3a8552efcaca2742fe2df
Maxime Fabre <ehtnam6@gmail.com>
2013-01-07 04:35:04 +0100

    Add Service Provider

---
+++ src/Anahkiasen/Providers/Str.php
@@ -0,0 +1,38 @@
+<?php
+namespace Anahkiasen\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class StrServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['str'] = $this->app->share(function($app)
+        {
+            return new Str(new Pluralizer);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('str');
+    }
+
+}
